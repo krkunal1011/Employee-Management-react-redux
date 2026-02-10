2b6d65187f7acea07ab1566b9c36a6fdf49ed4d3
Kunal Kumar <kk10111978@gmail.com>
2026-02-10 20:02:42 +0530

    feat: add employee Redux slice with CRUD reducers

---
+++ employee-manager/src/redux/employeeSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+const intitialState ={
+    list: []
+};
+
+const employeeSlice = createSlice({
+    name: "employees",
+    initialState,
+    reducers:{
+        addEmployee: {
+            reducers(state, action) {
+               state.list.push(action.payload);
+            },
+            prepare(data) {
+                return{
+                    payload: {
+                        id: nanoid(),
+                        ...data,
+                    },
+                };
+            },
+            
+        },
+
+        updateEmployee(state, action) {
+      const index = state.list.findIndex(
+        (emp) => emp.id === action.payload.id
+      );
+
+      if (index !== -1) {
+        state.list[index] = action.payload;
+      }
+    },
+
+    deleteEmployee(state, action) {
+      state.list = state.list.filter(
+        (emp) => emp.id !== action.payload
+      );
+    },
+
+
+    },
+
+})
+
+export const {
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
+} = employeeSlice.actions;
+
+export default employeeSlice.reducer;
