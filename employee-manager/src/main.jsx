040a8d94fe20b0b4d5cbd2f9e671cb3618ddf019
Kunal Kumar <kk10111978@gmail.com>
2026-02-10 19:50:33 +0530

    feat: wrap app with Redux Provider

--- employee-manager/src/main.jsx
+++ employee-manager/src/main.jsx
@@ -7,7 +7,7 @@ import { store } from "./app/store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-   <Provider store={store}>
+  <Provider store={store}>
     <App/>
-    </Provider>
+  </Provider>
 );
